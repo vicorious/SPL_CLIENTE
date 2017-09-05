fbf488de0e49bac282311919b0c495e38e5cdf99
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2017-09-05 14:48:12 -0500

    Actualizacion casos de prueba

---
+++ src/co/com/vicorious/pruebas/persistencia/dbobjects/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Este paquete de prueba contiene todos los objetos a consumir en la base de datos QUE NO SEAN TABLAS
+ * 
+ * 
+ */
+
+package co.com.vicorious.pruebas.persistencia.dbobjects;
---
+++ src/co/com/vicorious/pruebas/persistencia/main/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Este paquete contiene todas las pruebas unitarias de SPL
+ * 
+ */
+package co.com.vicorious.pruebas.persistencia.main;
