699ed3eca952c62c183ff92f308a96aa1dfbc568
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2017-09-05 14:49:24 -0500

    Fixes

--- src/co/com/vicorious/pruebas/persistencia/dbobjects/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Este paquete de prueba contiene todos los objetos a consumir en la base de datos QUE NO SEAN TABLAS
- * 
- * 
- */
-
-package co.com.vicorious.pruebas.persistencia.dbobjects;
